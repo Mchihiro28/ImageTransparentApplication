ea108370ecf5d6e47b2759a9738e2d1ad4715754
Mchihiro28 <matunagachihiro@gmail.com>
2022-02-15 21:37:11 +0900

    Initial commit

---
+++ app/src/main/java/matunagachihiro/spiritaway/com/example/imagetransparentapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package matunagachihiro.spiritaway.com.example.imagetransparentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
